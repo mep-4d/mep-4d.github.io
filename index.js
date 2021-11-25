494c83600e88c0be020eea1310ee34ffdd467f56
mep-4d <93514502+mep-4d@users.noreply.github.com>
2021-11-25 19:13:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,80 @@
-var app1 = new Vue({
-    el: '#app',
-    data: {
-        
-        value001    : 22,
-        value002    : 67,
-        value003    : 83,
-        value004    : 8,
-        
-        value005    :100, 
-        value006    :98, 
-        value007    :96,
-        value008    :100, 
-        value009    :84, 
-        value010    :77,
-        value011    :100, 
-        value012    :91, 
-        value013    :89,
-        value014    :100, 
-        value015    :100, 
-        value016    :92,
-        value017    :89,
-        value018    :100, 
-        value019    :100, 
-        value020    :92,
-        
-        classData   : "w3-container w3-red w3-padding-16",
-        styler      : false,
-        resource    : "TEST"
-        
-    }, // --- End of data --- //
+const app = Vue.createApp({
+
+    data() {
+        return {
+            select01   :"",
+            select02   :"",
+            select03   :"",
+            value001   :25,
+            value002   :50,
+            value003   :75,
+            value004   :100,
+            list01     :["AHU","RTU","MAU","HRU","VRF","EF","HVLS","UH","BMS","Sensors","Valves","Actuators"],
+            list02     :[],
+            list03     :[],
+            manuA      :["AHU's 01","AHU's 02","AHU's 03"],
+            manuB      :["RTU's 01","RTU's 02","RTU's 03"],
+            manuC      :["MAU's 01","MAU's 02","MAU's 03"],
+            manuAunitsA:["unitA","unitB","unitC"],
+            manuAunitsB:["unitD","unitE","unitF"],
+            manuAunitsC:["unitG","unitH","unitI"],
+            colorA     :"red",
+            show       : true,
+            resource   :"TEST"
+        };
+    },
+
     computed: {
-    }, // --- End of computed --- //
+        bar01Style() {
+            return {width: this.value001 + "%", backgroundColor: this.colorA}
+        },
+        bar02Style() {
+            return {width: this.value002 + "%"}
+        },
+        bar03Style() {
+            return {width: this.value003 + "%"}
+        },
+        bar04Style() {
+            return {width: this.value004 + "%"}
+        },
+    },
+
     methods: {
-    }, // --- End of methods --- //
-    mounted: {
+        //runFunc(event) {
+        //    console.log(event.target.value)
+        //},
+        reset() {
+            this.value001 = 0; this.value002 = 0; this.value003 = 0; this.value004 = 0; this.show = false;
+        },
+        log() {
+            console.log(this.select01, this.select02);
+             
+        },
+        listManufacturers() {
+            var m01 = this.manuA
+            var m02 = this.manuB
+            var m03 = this.manuC
+            if (this.select01.includes("AHU")) {
+                this.list02 = m01          
+            } else if (this.select01.includes("RTU")) {
+                this.list02 = m02   
+            } else if (this.select01.includes("MAU")) {
+                this.list02 = m03      
+            }
+        },
+        listModels() {
+            var aa = this.manuAunitsA
+            var ab = this.manuAunitsB
+            var ab = this.manuAunitsC
+            if ((this.select01.includes("AHU")) && (this.select02.includes("01"))) {
+                this.list03 = aa          
+            } else if ((this.select01.includes("AHU")) && (this.select02.includes("02"))) {
+                this.list03 = ab   
+            } else if ((this.select01.includes("AHU")) && (this.select02.includes("03"))) {
+                this.list03 = ac     
+            }
+        }
     }
 });
+
+app.mount('#sectionA');
