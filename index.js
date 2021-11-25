0709555eb20f1c669822ad42bb8adc6ac67ab9a7
mep-4d <93514502+mep-4d@users.noreply.github.com>
2021-11-25 19:25:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const app = Vue.createApp({
 
     data() {
         return {
-            select01   :"",
-            select02   :"",
-            select03   :"",
+            //select01   :"",
+            //select02   :"",
+            //select03   :"",
             value001   :25,
             value002   :50,
             value003   :75,
@@ -28,39 +28,6 @@ const app = Vue.createApp({
     },
 
     methods: {
-        //runFunc(event) {
-        //    console.log(event.target.value)
-        //},
-        reset() {
-            this.value001 = 0; this.value002 = 0; this.value003 = 0; this.value004 = 0; this.show = false;
-        },
-        log() {
-            console.log(this.select01, this.select02);
-        },
-        listManufacturers() {
-            var m01 = this.manuA
-            var m02 = this.manuB
-            var m03 = this.manuC
-            if (this.select01.includes("AHU")) {
-                this.list02 = m01          
-            } else if (this.select01.includes("RTU")) {
-                this.list02 = m02   
-            } else if (this.select01.includes("MAU")) {
-                this.list02 = m03      
-            }
-        },
-        listModels() {
-            var aa = this.manuAunitsA
-            var ab = this.manuAunitsB
-            var ab = this.manuAunitsC
-            if ((this.select01.includes("AHU")) && (this.select02.includes("01"))) {
-                this.list03 = aa          
-            } else if ((this.select01.includes("AHU")) && (this.select02.includes("02"))) {
-                this.list03 = ab   
-            } else if ((this.select01.includes("AHU")) && (this.select02.includes("03"))) {
-                this.list03 = ac     
-            }
-        }
     }
 });
 
