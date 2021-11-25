0d8d57d2cf6d8f792e0066347cbd241a8febfbb6
mep-4d <93514502+mep-4d@users.noreply.github.com>
2021-11-25 19:05:42 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+var app1 = new Vue({
+    el: '#app',
+    data: {
+        
+        value001    : 22,
+        value002    : 67,
+        value003    : 83,
+        value004    : 8,
+        
+        value005    :100, 
+        value006    :98, 
+        value007    :96,
+        value008    :100, 
+        value009    :84, 
+        value010    :77,
+        value011    :100, 
+        value012    :91, 
+        value013    :89,
+        value014    :100, 
+        value015    :100, 
+        value016    :92,
+        value017    :89,
+        value018    :100, 
+        value019    :100, 
+        value020    :92,
+        
+        classData   : "w3-container w3-red w3-padding-16",
+        styler      : false,
+        
+    }, // --- End of data --- //
+    computed: {
+    }, // --- End of computed --- //
+    methods: {
+    }, // --- End of methods --- //
+    mounted: {
+    }
+});
