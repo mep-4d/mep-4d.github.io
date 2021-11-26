8dd46419bd70ae39554a30ec8685935b2d7b25bf
mep-4d <93514502+mep-4d@users.noreply.github.com>
2021-11-26 09:31:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,10 @@ const app = Vue.createApp({
             value002   :50,
             value003   :75,
             value004   :100,
-            list01     :["AHU","RTU","MAU","HRU","VRF","EF","HVLS","UH","BMS","Sensors","Valves","Actuators"],
+            list01     :[],
             list02     :[],
             list03     :[],
             manuA      :["AHU's 01","AHU's 02","AHU's 03"],
-            manuB      :["RTU's 01","RTU's 02","RTU's 03"],
-            manuC      :["MAU's 01","MAU's 02","MAU's 03"],
             manuAunitsA:["unitA","unitB","unitC"],
             manuAunitsB:["unitD","unitE","unitF"],
             manuAunitsC:["unitG","unitH","unitI"],
@@ -29,6 +27,18 @@ const app = Vue.createApp({
     },
 
     methods: {
+        
+        listItems() {
+            var mec = ["AHU","FCU","VAV","Boiler","Chiller","Pump"]
+            if (this.selectA.includes("Mech")) {
+                this.list01 = mec          
+            } else if (this.selectA.includes("RTU")) {
+                this.list01 = mec   
+            } else if (this.selectA.includes("MAU")) {
+                this.list01 = mec      
+            }
+        },
+        
     }
 });
 
