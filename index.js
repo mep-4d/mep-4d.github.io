e41d7605e71a40af8479ce22849ec75b8304254c
mep-4d <93514502+mep-4d@users.noreply.github.com>
2021-11-26 09:50:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ const app = Vue.createApp({
             selectB    :"",
             selectC    :"",
             selectD    :"",
-            value001   :25,
-            value002   :50,
-            value003   :75,
-            value004   :100,
             list01     :[],
-            list02     :[],
-            list03     :[],
             manuA      :["AHU's 01","AHU's 02","AHU's 03"],
             manuAunitsA:["unitA","unitB","unitC"],
             manuAunitsB:["unitD","unitE","unitF"],
@@ -36,7 +30,8 @@ const app = Vue.createApp({
                 this.list01 = mec   
             } else if (this.selectA.includes("MAU")) {
                 this.list01 = mec      
-            }
+            } 
+            console.log(mec)
         },
         
     }
