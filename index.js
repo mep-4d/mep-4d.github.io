9c58f25ce3d8c87b13abd9d01f08613766f3594b
mep-4d <93514502+mep-4d@users.noreply.github.com>
2021-11-27 08:39:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const app = Vue.createApp({
             selectC    :"",
             selectD    :"",
             list01     :[],
-            listsA     :["AHU","FCU","VAV","Boiler","Chiller","Pump"],
+            listsA     :["AHU","FCU","VAV","Extract Fan","Supply Fan","Smoke Fan","Boiler","Chiller","Pump","Valve","Commissioning Point"],
             listsB     :["DB","Switch Board","Socket Outlet","Fused Outlet","Isolator"],
             listsC     :["CHW Thermal Energy","LTHW Thermal Energy","Electricity","Water","Gas"],
             listsD     :["Luminaire","Wall Pad","LCM","PIR","Switch","LED Fixture"],
-            listsE     :["Main Enclosure","Enclosure","Terminal Unit","Controller","I/O Module","Translator","Sensor","Switch","Network Item"],
+            listsE     :["Main Enclosure","Enclosure","Terminal Unit","Controller","I/O Module","Translator","Sensor","Switch","Actuator","Network Item"],
             listsF     :["Main Enclosure","Enclosure","Touch Panel","Interface","Voice","TV","Speaker","Switch","Network Item"],
+            listsG     :["Panel","Heat Detector","Smoke Detector","Alarm Point","Relay Interface","Actuator","Vent","Network Item"],
+            listsH     :["Panel","Intrusion Detector","IR Beam","Alarm Point","Relay Interface","Network Item"],
             resource   :"TEST"
         };
     },
@@ -30,12 +32,16 @@ const app = Vue.createApp({
             var d = this.listsD
             var e = this.listsE
             var f = this.listsF
+            var g = this.listsG
+            var h = this.listsH
             if (this.selectA == "1") {this.list01 = a; console.log("mec selected")} 
             else if (this.selectA == "2") {this.list01 = b; console.log("lec selected")} 
             else if (this.selectA == "3") {this.list01 = c; console.log("mtr selected")} 
             else if (this.selectA == "4") {this.list01 = d; console.log("ltg selected")}
             else if (this.selectA == "5") {this.list01 = e; console.log("bms selected")} 
             else if (this.selectA == "6") {this.list01 = f; console.log("avi selected")}
+            else if (this.selectA == "7") {this.list01 = g; console.log("fir selected")}
+            else if (this.selectA == "8") {this.list01 = h; console.log("sec selected")} 
         }
         
     }
