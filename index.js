2a629ac79684d90d95711ea5ba9110c4f572ea5f
mep-4d <93514502+mep-4d@users.noreply.github.com>
2021-11-27 07:53:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = Vue.createApp({
             list01     :[],
             listsA     :["AHU","FCU","VAV","Boiler","Chiller","Pump"],
             listsB     :["DB","Switch Board","Socket Outlet","Fused Outlet","Isolator"],
+            listsC     :["CHW Thermal Energy","LTHW Thermal Energy","Electricity","Water","Gas"],
+            listsD     :["Luminaire","Wall Pad","LCM","PIR","Switch","LED Fixture"],
             resource   :"TEST"
         };
     },
@@ -22,14 +24,22 @@ const app = Vue.createApp({
             console.log("selection");
             var a = this.listsA
             var b = this.listsB
+            var c = this.listsC
+            var d = this.listsD
             if (this.selectA == "1") {
                 this.list01 = a      
-                console.log("mech selected")
+                console.log("mec selected")
             } else if (this.selectA == "2") {
                 this.list01 = b      
-                console.log("elec selected")
+                console.log("lec selected")
+            } else if (this.selectA == "3") {
+                this.list01 = c      
+                console.log("mtr selected")
+            } else if (this.selectA == "4") {
+                this.list01 = d      
+                console.log("ltg selected")
             }
-        }
+        };
         
     }
    
