3de5c894c95ca489d6daa17d634da7b3b523ccd6
mep-4d <93514502+mep-4d@users.noreply.github.com>
2021-12-05 06:52:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,6 @@ const app = Vue.createApp({
 		res.json().then(data=>{
 			this.database = data
 		})//.catch(err => console.log(err))}else{console.log("nah")}
-	        })
                 })
             },
 	    
