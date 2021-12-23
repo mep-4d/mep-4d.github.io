69026b598ef8dfdbe8b274fa8cd179aafa4d92d1
mep-4d <93514502+mep-4d@users.noreply.github.com>
2021-12-23 15:21:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ const app = Vue.createApp({
 	    var table = new Tabulator("#works-table", {
             rowClick:function(e, row){
             console.log("CLICKED YO")
-            }
+            },
  	    data:this.tabledataW,
  	    layout:"fitColumns",
  	    columns:[ //Define Table Columns
