7c53e40f9afa947e929cff18a725184c46cafdf1
mep-4d <93514502+mep-4d@users.noreply.github.com>
2022-04-12 10:13:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -78,6 +78,27 @@ const app = Vue.createApp({
             setTimeout(this.compileW, 4000)
             },
 	    
+	    syntaxHighlight(json) {
+            json = JSON.stringify(json, undefined, 4)
+            json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+            json = json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+                var cls = 'number'
+                if (/^"/.test(match)) {
+                    if (/:$/.test(match)) {
+                        cls = 'key'
+                    } else {
+                        cls = 'string'
+                    }
+                } else if (/true|false/.test(match)) {
+                    cls = 'boolean'
+                } else if (/null/.test(match)) {
+                    cls = 'null'
+                }
+                return '<span class="' + cls + '">' + match + '</span>'
+            })
+            return json
+            }, // --- End of syntaxHighlight --- //
+	    
 	    listWorksA() {
 	    var a = this.selectW1
 	    if (a === "1") {this.createTableW1()}
