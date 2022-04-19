7c909b534cde1bc1b813b904d3ccc9cf23b0de4e
mep-4d <93514502+mep-4d@users.noreply.github.com>
2022-04-19 09:03:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -350,8 +350,8 @@ const app = Vue.createApp({
 	    var b = this.selectNB
 		console.log(a, b)
 	    if (a == "1" && b == "Panel") {
-		    this.helpN1 = "Input the panel identifier in the text box exactly as its referenced i.e MCC-01-01 including hyphens or underscores" +
-			    "Replace any blank spaces with a hyphen i.e. CP 01 01 would become CP-01-01"
+		    this.helpN1 = "Input the panel identifier in the text box exactly as its referenced i.e MCC-01-01 including hyphens or underscores "+
+			     "Replace any blank spaces with a hyphen i.e. CP 01 01 would become CP-01-01"
             }
 	    }
         
