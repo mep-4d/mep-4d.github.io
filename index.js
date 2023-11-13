9f231e70ea83eda5f55b2d5d0d6cde28ab9c9090
Nick W <93514502+mep-4d@users.noreply.github.com>
2023-11-13 09:23:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var controller = new demoApp();
 // JavaScript "class" containing the model, providing controller "methods" for the HTML view
 function demoApp() {
     console.log("Creating controller/model");
+    this.authenticate();
 
     var URL = "https://attain.aeronlabs.com/";
 
