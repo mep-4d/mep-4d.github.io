6ab3c2ac11228411912e4524ebe91b85ed1a96f4
mep-4d <bems.consulting@gmail.com>
2023-11-13 11:49:21 +0200

    iteration1

--- index.js
+++ index.js
@@ -1,54 +1,37 @@
-// by nick W
-// Execute in strict mode to prevent some common mistakes
-"use strict";
+// console.log("Creating controller/model");
+const URL = "https://attain.aeronlabs.com/";
+const ACCESS_TOKEN_STORAGE_KEY = "token";
+const SPACER = ":";
 
-// Create object for use by the HTML page
-var controller = new demoApp();
-
-// JavaScript "class" containing the model, providing controller "methods" for the HTML view
-function demoApp() {
-    console.log("Creating controller/model");
-    var URL = "https://attain.aeronlabs.com/";
-
-    this.authenticate = function () {
-        console.log("RUNNING AUTH");
-        const ACCESS_TOKEN_STORAGE_KEY = "token";
-        const token = localStorage.getItem(ACCESS_TOKEN_STORAGE_KEY);
-        console.log(token)
-        if (token === null) {
-            //const URL = "https://attain.aeronlabs.com/";
-            localStorage.setItem("prevPage", window.location.href);
-            window.location.href = URL + "auth";
-            return;
-        }
-    },
-        
-    this.logout = function () {
-        //const URL = "https://attain.aeronlabs.com/";
-        const ACCESS_TOKEN_STORAGE_KEY = "token";
-        const token = localStorage.getItem(ACCESS_TOKEN_STORAGE_KEY);
-        localStorage.removeItem(ACCESS_TOKEN_STORAGE_KEY);
-        localStorage.removeItem("prevPage");
+function authenticate() {
+    const token = localStorage.getItem(ACCESS_TOKEN_STORAGE_KEY);
+    if (token === null) {
+        localStorage.setItem("prevPage", window.location.href);
         window.location.href = URL + "auth";
+        return;
     }
-
-    this.userLogin = function () {
-        var userPw = $("#userPword").val(); // GET PASSWORD
-        var fname = $("#userFname").val(); // GET FIRST NAME INPUT
-        var lname = $("#userLname").val(); // GET LAST NAME INPUT
-        const spacer = ":"
-        console.log(fname, lname, userPw);
-        // SEND DATA TO AUTH ENDPOINT
-        var url = URL + `auth?creds=${fname+spacer+userPw}`
-        $.ajax(url, { type: "GET", data: {}, success: onSuccess });
-        function onSuccess(data) { // RETURNS TOKEN -> STORE IN LOCAL OBJECT
-            console.log(data)
-            var self = globalThis;
-            self.things= data;
-            }
-        //this.match()
-    }
-
-    this.authenticate()
-
 }
+    
+function logout() {
+    const token = localStorage.getItem(ACCESS_TOKEN_STORAGE_KEY);
+    localStorage.removeItem(ACCESS_TOKEN_STORAGE_KEY);
+    localStorage.removeItem("prevPage");
+    window.location.href = URL + "auth";
+}
+
+function userLogin() {
+    const password = $("#password").val(); // GET PASSWORD
+    const username = $("#username").val(); // GET FIRST NAME INPUT
+    // SEND DATA TO AUTH ENDPOINT
+    const _url = URL + `auth?creds=${username+SPACER+password}`
+    $.ajax(_url, { type: "GET", data: {}, success: onSuccess });
+    function onSuccess(data) { // RETURNS TOKEN -> STORE IN LOCAL OBJECT
+        console.log(data)
+        const self = globalThis;
+        self.things= data;
+        }
+}
+
+$(function() {
+    authenticate();
+})
